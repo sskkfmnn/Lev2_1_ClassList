fd7cad514a553ed074d90377c63bc51caf4dba03
sskkfmnn <sskkfmnn@gmail.com>
2023-02-20 15:55:29 +0100

    Lev2_1

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+console.log("");
+
+function changeHeadline(){
+    document.getElementById("headline").classList.add("bgc");
+}
+
+function resetHeadline(){
+    document.getElementById("headline").classList.remove("bgc");
+}
